836e593ee8afe19890284399e337043e85877b5b
Anwar Ahmed <nileking2007@gmail.com>
2025-08-10 23:52:39 +0300

    fix : solve typescript errors

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 // Main module exports for the concurrency library
 
 // Export core concurrency primitives
-export { default as Mutex } from './mutex.js';
-export { default as Semaphore } from './semaphore.js';
+export { default as Mutex } from "./mutex.js";
+export { default as Semaphore } from "./semaphore.js";
 
 // Export bank account implementation
-export { SafeBankAccount } from './bank-account.js';
+export { SafeBankAccount } from "./bank-account.js";
 
 // Export database connection pool
-export { DatabaseConnectionPool } from './db-connection.js';
+export { DatabaseConnectionPool } from "./db-connection.js";
 
 // Export all types and interfaces
-export {
+export type {
   Action,
   MessageType,
   Message,
-  Messages,
   GitHubUser,
   GitHubRepo,
-  OpenAIResponse
-} from './types.js';
+  OpenAIResponse,
+} from "./types.js";
+
+// Export value exports (like Messages) separately
+export { Messages } from "./types.js";
 
 // Note: API implementations are not exported by default as they contain
 // class definitions that users might want to instantiate directly
